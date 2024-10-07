7b17a383a13f327bb5fd2d5852a35a9c4bd026d1
monsteruser2021 <monsteruser2021@fmail.com>
2024-10-07 10:53:33 -0400

    Poniendo items del footer (lo acomodo en mi casa)

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -3,9 +3,20 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div className='flex w-full justify-center text-xs h-36 bg-orange-300 dark:bg-quinary '>
+    <div className='h-36 bg-orange-300 dark:bg-quinary '>
+      <div className='text-xs flex w-full justify-center'>
       <span>Contactos</span>
       <span>&copy; {new Date().getFullYear() }</span>
+      </div>
+      <div className='tex'>
+        <ol>
+          <li>Hogar</li>
+          <li>Populares</li>
+          <li>Destinos</li>
+          <li>Blog</li>
+          <li>contacto</li>
+        </ol>
+      </div>
     </div>
   )
 }
