e6241b52102baa06a587757c60bade861f42f80e
Javier <lbpf2021jesr@gmail.com>
2024-10-13 16:43:07 -0400

    Main

--- app/components/main/Main.tsx
+++ app/components/main/Main.tsx
@@ -1,14 +1,13 @@
 import Image from "next/image";
-import Img1 from '../../../public/avila.jpg'
-import Img2 from '../../../public/avilaNight.jpg'
-import Img3 from '../../../public/petare.jpg'
-import Img4 from '../../../public/catatumbo.jpg'
-import Img5 from '../../../public/cumana.jpg'
+import Img1 from '../../../public/imgDay1.jpg'
+import Img2 from '../../../public/imgDay2.jpg'
+import Img3 from '../../../public/imgDay3.jpg'
+import Img4 from '../../../public/imgDay4.jpg'
+import Img5 from '../../../public/imgDay5.jpg'
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import React from 'react';
-import { SeeAllButton } from '../buttons/Buttons';
 
 const ImageList = [
   {
@@ -56,20 +55,19 @@ export default function Main() {
   return <>
     <div>
       {/* La ventana donde el usuario introduce su destino y fecha de llegada */}
-      <div className=' flex flex-col items-center justify-center inset-72 bg-tertiary/75 backdrop-blur-md w-[50%] h-[200px] left-[25%] rounded-3xl ml-2 absolute z-20 dark:bg-[#ac3636dd] drop-shadow-md'>
-        <h1 className='mb-5 mt-6 text-5xl p-1 bg-white rounded-lg w-[80%] text-center h-[60px] font-black dark:bg-[#6d2a2a] dark:text-white'>Bienvenido a Ven-Turista!</h1>
+      <div className=' flex flex-col items-center inset-72 bg-tertiary/75 backdrop-blur-md w-[70%] h-[200px] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-3xl ml-2 absolute z-20 dark:bg-[#ac3636dd] drop-shadow-md'>
+        <h1 className='mb-5 mt-6 text-4xl p-1 bg-white rounded-lg w-[80%] text-center h-[60px] font-black dark:bg-[#6d2a2a] dark:text-white'>Bienvenido a Ven-Turista!</h1>
         <div className="flex text-center">
-          <div><p className="text-white">Ubicación</p><input type="name" className='w-[200px] h-[35px] rounded-lg ml-2' placeholder="Ingresar Destino" /></div>
-          <div><p className="text-white">Fecha de Llegada</p><input type="date" className='w-[200px] h-[35px] rounded-lg ml-2' /></div>
-          <div><p className="text-white">Fecha de Salida</p><input type="date" className='w-[200px] h-[35px] rounded-lg ml-2' /></div>
-          <div><p className="text-white">Precio</p><input type="name" className='w-[100px] h-[35px] rounded-lg ml-2' placeholder="Min" /></div>
-          <div><p className="text-white">Precio</p><input type="name" className='w-[100px] h-[35px] rounded-lg ml-2' placeholder="Max" /></div>
+          <div><p className="text-white">Ubicación</p><input type="name" className='w-[150px] h-[35px] rounded-lg ml-2' placeholder="Ingresar Destino" /></div>
+          <div><p className="text-white">Fecha de Llegada</p><input type="date" className='w-[130px] h-[35px] rounded-lg ml-2' /></div>
+          <div><p className="text-white">Fecha de Salida</p><input type="date" className='w-[130px] h-[35px] rounded-lg ml-2' /></div>
+          <div><p className="text-white">Precio</p><input type="name" className='w-[50px] h-[35px] rounded-lg ml-2' placeholder="Min" /></div>
+          <div><p className="text-white">Precio</p><input type="name" className='w-[50px] h-[35px] rounded-lg ml-2' placeholder="Max" /></div>
         </div>
         <button>Buscar</button>
-        <SeeAllButton />
       </div>
       {/* La ventana donde el usuario introduce su destino y fecha de llegada */}
-      <div className='z-10 absolute w-[100%] h-[100%] bg-primary opacity-55 dark:bg-quinary'></div>
+      <div className='z-10 absolute w-[100%] h-[100%] bg-primary opacity-45 dark:bg-quinary'></div>
       {/* carrousel*/}
       <div className='z-0 relative'>
         <Slider {...settings}>
