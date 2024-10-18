68652417d7c95dae560980d963d38c56ecfb20c4
Drago-01 <alfredo.yonelwi@gmail.com>
2024-10-18 14:53:26 -0400

    modificacion de destinos todavia falta

--- app/components/destinations/Destinations.tsx
+++ app/components/destinations/Destinations.tsx
@@ -5,35 +5,33 @@ import Img3 from '../../../public/Destinations3.jpg'
 import Img4 from '../../../public/Destinations4.jpg'
 import Img5 from '../../../public/Destinations5.jpg'
 import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import Image from 'next/image'
 
 const ImageList = [
   {
     id: 1,
     img: Img1,
-    display: "bg-white ",
+    display: "w-full h-96",
   },
   {
     id: 2,
     img: Img2,
-    display: "bg-white",
+    display: "w-full h-96 ",
   },
   {
     id: 3,
     img: Img3,
-    display: "bg-white",
+    display: "w-full h-96",
   },
   {
     id: 4,
     img: Img4,
-    display: "bg-white",
+    display: "w-full h-96",
   },
   {
     id: 5,
     img: Img5,
-    display: "bg-white",
+    display: "w-full h-96",
   },
 ]
 
@@ -46,17 +44,22 @@ const Destinations = () => {
     slidesToScroll: 1,
   };
   return (
-    <div className='w-screen h-screen inline-block bg-primary/40 dark:bg-quinary/70 dark:text-gray-100 overflow-hidden items-center'>
+    <div className='w-screen h-screen bg-primary/40 dark:bg-quinary/70 dark:text-gray-100 overflow-hidden items-center font-inter'>
       <h1 className='font-bold tracking-widest text-xl lg:text-5xl mx-[1rem] my-[2rem] text-center'>DESTINOS</h1>
-      <div className='w-screen h-1/2 inline-block'>
-        <Slider {...settings}>
+      <div className='w-screen h-2/3'>
+        <Slider {...settings} className='h-full'>
           {ImageList.map((data, key) => (
-            <div key={key}>
+            <div key={key} className='w-full p-8'>
               <Image
                 src={data.img}
                 alt=""
                 className={data.display}
               />
+              <div className='w-full p-8 bg-gray-100 dark:bg-black'>
+                <h1 className='text-2xl font-black tracking-wider'>Chukuto Planet</h1>
+                <br></br>
+                <p>Lido Poodle</p>
+              </div>
             </div>
           ))}
         </Slider>
